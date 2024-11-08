eab3f47387cf702a5cecef181214f5b558ff3db0
MASTERDOSNOOBS <jairderioboeing@gmail.com>
2024-11-08 08:35:38 -0300

    feat: add transaction type-badge

--- app/transactions/_columns/index.tsx
+++ app/transactions/_columns/index.tsx
@@ -1,9 +1,8 @@
 "use client";
 
-import { Badge } from "@/app/_components/ui/badge";
-import { Transaction, TransactionType } from "@prisma/client";
+import { Transaction } from "@prisma/client";
 import { ColumnDef } from "@tanstack/react-table";
-import { CircleIcon } from "lucide-react";
+import TransactionTypeBadge from "../_components/type-bagde";
 
 export const transactionColumns: ColumnDef<Transaction>[] = [
   {
@@ -14,28 +13,7 @@ export const transactionColumns: ColumnDef<Transaction>[] = [
     accessorKey: "type",
     header: "Tipo",
     cell: ({ row: { original: transaction } }) => {
-      if (transaction.type === TransactionType.DEPOSIT) {
-        return (
-          <Badge className="bg-muted font-bold text-primary hover:bg-muted">
-            <CircleIcon className="mr-2 fill-primary" size={10} />
-            Depósito
-          </Badge>
-        );
-      }
-      if (transaction.type === TransactionType.EXPENSE) {
-        return (
-          <Badge className="bg-danger bg-opacity-10 font-bold text-danger">
-            <CircleIcon className="mr-2 fill-danger" size={10} />
-            Despesa
-          </Badge>
-        );
-      }
-      return (
-        <Badge className="bg-muted font-bold text-white hover:bg-muted">
-          <CircleIcon className="mr-2 fill-white" size={10} />
-          Investimento
-        </Badge>
-      );
+      <TransactionTypeBadge transaction={transaction} />;
     },
   },
   {
