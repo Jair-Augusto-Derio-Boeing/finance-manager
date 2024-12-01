654f1192529b690081dcd2e1a50c78abd3f9521f
Jair Augusto Dério Boeing <jairderioboeing@gmail.com>
2024-12-01 15:29:22 -0300

    feat: add last transactions component

--- app/_data/get-dashboard/index.ts
+++ app/_data/get-dashboard/index.ts
@@ -66,6 +66,11 @@ export const getDashboard = async (month: string) => {
       (Number(category._sum.amount) / Number(transactionsTotal)) * 100,
     ),
   }));
+  const lastTransactions = await db.transaction.findMany({
+    where,
+    orderBy: { date: "desc" },
+    take: 10,
+  });
   return {
     balance,
     depositsTotal,
@@ -73,5 +78,6 @@ export const getDashboard = async (month: string) => {
     expensesTotal,
     typesPercentage,
     totalExpensePerCategory,
+    lastTransactions,
   };
 };
